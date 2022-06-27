992e0c51b7b14a48363f712cd02a31e9e52cf911
Shubom123 <shubomnath365@gmail.com>
2022-06-27 09:49:52 +0530

    Added: new functionality

--- src/com/addressbook/AddInfo.java
+++ src/com/addressbook/AddInfo.java
@@ -1,2 +1,44 @@
-package com.addressbook;public class AddInfo {
+package com.addressbook;
+import java.util.Scanner;
+public class AddInfo {
+    public void addContact(){
+        Scanner sc= new Scanner(System.in);
+        AddressBook contact=new AddressBook();
+        System.out.println("Enter First Name: ");
+        String firstname = sc.next();
+        System.out.println("Enter last Name: ");
+        String lastname = sc.next();
+        System.out.println("Enter Address: ");
+        String adderss = sc.next();
+        System.out.println("Enter City: ");
+        String city = sc.next();
+        System.out.print("Enter State: ");
+        String state = sc.next();
+        System.out.print("Enter Zip Code: ");
+        int zip = sc.nextInt();
+        System.out.print("Enter Pnone Number: ");
+        long phoneNumber = sc.nextLong();
+        System.out.println("Enter Email: ");
+        String email = sc.next();
+
+        contact.setFirstName(firstname);
+        contact.setLastName(lastname);
+        contact.setAddress(adderss);
+        contact.setCity(city);
+        contact.setState(state);
+        contact.setZip(zip);
+        contact.setPhoneNumber(phoneNumber);
+        contact.setEmail(email);
+
+        System.out.println(contact.getLastName());
+        System.out.println(contact.getFirstName());
+        System.out.println(contact.getAddress());
+        System.out.println(contact.getCity());
+        System.out.println(contact.getState());
+        System.out.println(contact.getZip());
+        System.out.println(contact.getPhoneNumber());
+        System.out.println(contact.getEmail());
+
+    }
+
 }
--- src/com/addressbook/AddressBookMain.java
+++ src/com/addressbook/AddressBookMain.java
@@ -2,24 +2,9 @@ package com.addressbook;
 
 public class AddressBookMain {
     public static void main(String args[]) {
-        System.out.println("Welcome to Address Book");
-        AddressBook contact=new AddressBook();
-        contact.setFirstName("Shubom");
-        System.out.print(contact.getFirstName());
-        contact.setLastName("Nath");
-        System.out.print(contact.getLastName());
-        contact.setAddress("Pasighat, East Siang, Arunachal Pradesh");
-        System.out.println(contact.getAddress());
-        contact.setCity("Pasighat");
-        System.out.print(contact.getCity());
-        contact.setState("Arunachal Pradesh");
-        System.out.print(contact.getState());
-        contact.setZip(791102);
-        System.out.print(contact.getZip());
-        contact.setPhoneNumber(8288894252L);
-        System.out.println(contact.getPhoneNumber());
-        contact.setEmail("shubomnath88@gmail.com");
-        System.out.println(contact.getEmail());
+        AddInfo addInfo = new AddInfo();
+        addInfo.addContact();
+
     }
 
 }
