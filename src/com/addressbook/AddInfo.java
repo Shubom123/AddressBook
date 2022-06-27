f0ace9e5fa25b484745795c1a7795e50f742e72c
Shubom123 <shubomnath365@gmail.com>
2022-06-27 10:00:59 +0530

    Added: Edit functionality added

--- src/com/addressbook/AddInfo.java
+++ src/com/addressbook/AddInfo.java
@@ -1,6 +1,9 @@
 package com.addressbook;
 import java.util.Scanner;
+import java.util.ArrayList;
 public class AddInfo {
+    AddressBook contact=new AddressBook();
+    ArrayList<AddressBook> array = new ArrayList<AddressBook>();
     public void addContact(){
         Scanner sc= new Scanner(System.in);
         AddressBook contact=new AddressBook();
@@ -29,6 +32,7 @@ public class AddInfo {
         contact.setZip(zip);
         contact.setPhoneNumber(phoneNumber);
         contact.setEmail(email);
+        array.add(contact);
 
         System.out.println(contact.getLastName());
         System.out.println(contact.getFirstName());
@@ -39,6 +43,68 @@ public class AddInfo {
         System.out.println(contact.getPhoneNumber());
         System.out.println(contact.getEmail());
 
-    }
 
+    }
+    public void editPerson(){
+        System.out.println("Enter First name: ");
+        Scanner sc1 = new Scanner(System.in);
+        String firstName = sc1.next();
+        if(firstName.equals(contact.getFirstName())){
+            System.out.println("\nChoose attribute you want to change:");
+            System.out.println("1.First Name\n2.Last Name\n3.Address\n4.City\n5.State\n6.ZipCode\n7.Phone Number\n8.Email");
+            int choice = sc1.nextInt();
+
+            switch (choice){
+                case 1:
+                    System.out.println("Enter the correct first Name :");
+                    firstName = sc1.next();
+                    contact.setFirstName(firstName);
+                    break;
+                case 2:
+                    System.out.println("Enter the correct Last Name :");
+                    String lastName = sc1.next();
+                    contact.setLastName(lastName);
+                    break;
+                case 3:
+                    System.out.println("Enter the correct Address :");
+                    String address = sc1.next();
+                    contact.setAddress(address);
+                    break;
+                case 4:
+                    System.out.println("Enter the correct City :");
+                    String city = sc1.next();
+                    contact.setCity(city);
+                    break;
+                case 5:
+                    System.out.println("Enter the correct State :");
+                    String state = sc1.next();
+                    contact.setState(state);
+                    break;
+                case 6:
+                    System.out.println("Enter the correct Zip Code :");
+                    int zip = sc1.nextInt();
+                    contact.setZip(zip);
+                    break;
+                case 7:
+                    System.out.println("Enter the correct Phone Number :");
+                    long phoneNumber = sc1.nextLong();
+                    contact.setPhoneNumber(phoneNumber);
+                    break;
+                case 8:
+                    System.out.println("Enter the correct Email :");
+                    String email = sc1.next();
+                    contact.setEmail(email);
+                    break;
+            }
+        }
+        array.add(contact);
+        System.out.println(contact.getLastName());
+        System.out.println(contact.getFirstName());
+        System.out.println(contact.getAddress());
+        System.out.println(contact.getCity());
+        System.out.println(contact.getState());
+        System.out.println(contact.getZip());
+        System.out.println(contact.getPhoneNumber());
+        System.out.println(contact.getEmail());
+    }
 }
--- src/com/addressbook/AddressBookMain.java
+++ src/com/addressbook/AddressBookMain.java
@@ -1,9 +1,30 @@
 package com.addressbook;
-
+import java.util.Scanner;
 public class AddressBookMain {
     public static void main(String args[]) {
+        Scanner sc2= new Scanner(System.in);
+        System.out.println("Welcome to Address Book");
         AddInfo addInfo = new AddInfo();
-        addInfo.addContact();
+        int choose = sc2.nextInt();
+        boolean changes = true;
+
+        do{
+            switch (choose){
+
+                case 1:
+                    addInfo.addContact();
+                    break;
+                case 2:
+                    addInfo.editPerson();
+                    break;
+                case 3:
+                    changes = false;
+                    System.out.println("We are exiting");
+
+
+            }
+
+        }while (changes);
 
     }
 
